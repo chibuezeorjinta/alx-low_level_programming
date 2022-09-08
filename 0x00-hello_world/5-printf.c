3298382599241f0713aec31ac9ebe2232e5b1539
chibuezeorjinta <chibueze.orjinta.237245@unn.edu.ng>
2022-09-08 16:12:32 -0700

    betty!!!

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
-/*file description*/
 #include <stdio.h>
 /**
- * print given text
- * return 0 if successful
+ * main - entry point
+ * Return: always 0
  */
 int main(void)
-	/*main function*/
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	/*return 0*/
 	return (0);
 }
