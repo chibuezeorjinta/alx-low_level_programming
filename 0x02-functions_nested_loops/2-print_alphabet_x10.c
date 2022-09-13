7938c6efc72e70e7e78bde837824cc15a15e2a31
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2022-09-13 06:35:33 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - function name
+ * print_alphabet_x10 - function name
  *
  */
 void print_alphabet_x10(void)
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int i;
 	char a;
 
-	for (i = 0; i < 10; i++) 
+	for (i = 0; i < 10; i++)
 	{
 		for (a = 'a'; a <= 'z'; a++)
 		{
