3edde71671d8695912e72051afe25116db8f723e
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2022-09-13 07:37:16 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of input using %
+ * @i:input
+ * Return: return i % 10
+ */
+int print_last_digit(int i)
+{
+	int i;
+
+	return (i % 10);
+}
