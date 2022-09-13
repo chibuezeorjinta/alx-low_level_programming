79512b9d0869a1b888830c33d83a2c3211ad3214
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2022-09-13 06:45:38 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,3 +4,8 @@
  * _isalpha - check if input is an alphabet
  * @c: input
  * Return: 1 if true, 0 otherwise
+ */
+int _isalpha(int c)
+{
+		return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
