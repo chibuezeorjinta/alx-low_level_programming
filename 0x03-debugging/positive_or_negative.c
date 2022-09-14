bec5fb64d393d137ba4892eacf4266d1968054d9
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2022-09-14 06:12:50 -0700

    task 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Header entry line
+ * Return: equals 0
+ */
+void positive_or_negative(int i)
+{
+
+
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+}
+
