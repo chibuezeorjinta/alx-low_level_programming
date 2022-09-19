16f9b6df7bca35714d723ce82286847779703122
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2022-09-19 03:51:08 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_strlen - entry
+*@s: inpited string pointer
+*Return: return i
+*/
+int _strlen(char *s)
+{
+	int n, i;
+
+	for (n = 0; s[n] != '\0'; n++)
+	{
+		i = n + 1;
+	}
+	return (i);
+}
