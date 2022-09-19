0b6afc2bffc5086f56d610aed24cccdde58a47f2
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2022-09-19 09:01:53 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,6 +3,7 @@
 /**
  * print_array - entry
  * @n: given number
+ * @a: given string
  */
 void print_array(int *a, int n)
 {
@@ -10,7 +11,14 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf(", %d", a[i]);
+		if (i == n - 1)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf("%d, " a[i]);
+		}
 	}
 	printf("\n");
 }
