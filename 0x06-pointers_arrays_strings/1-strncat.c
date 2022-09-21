920ca9d6e8898567d9af514562e7d80370dd3dbe
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2022-09-21 12:02:50 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,7 +15,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; dest[i] != '\0'; i++)
 		i = i;
 
-	for (a = 0; a < n; a++)
+	for (a = 0; src[a] != '\0' && a < n; a++)
 	{
 		dest[i] = src[a];
 		i++;
