3f55dc9db9d42956a7a51f35d3cb53b407908c78
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2022-09-21 13:01:43 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,21 +10,12 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int a;
-	int i;
-	int j;
 
-	for (j = 0; src[j] != '\0'; j++)
-		j = j;
-
-
-	i = 0;
 
 	for (a = 0; a < n && src[a] != '\0'; a++)
-	{
-		dest[i] = src[a];
-		i++;
-	}
-	if (n >= j)
-		dest[j] = '\0';
+		dest[a] = src[a];
+
+	for ( ; a < n; a++)
+		dest[a] = '\0';
 	return (dest);
 }
