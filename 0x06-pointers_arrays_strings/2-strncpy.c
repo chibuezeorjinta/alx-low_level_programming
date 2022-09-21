a43580c0ba05aca3fd50e67bc95bf0a68d2f5964
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2022-09-21 12:41:48 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strncpy - entry to copy string
+ * @dest: receiving array
+ * @src: source array
+ * @n: limiting number
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+	int i;
+	int j;
+
+	for (j = 0; src[j] != '\0'; j++)
+		j = j;
+
+
+	i = 0;
+
+	for (a = 0; a < n && src[a] != '\0'; a++)
+	{
+		dest[i] = src[a];
+		i++;
+	}
+	if (n >= j)
+		dest[i] = '\0';
+	return (dest);
+}
