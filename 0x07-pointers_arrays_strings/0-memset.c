8f7d627c3a860ac001d42d0ac1bb44dc0898cdb7
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2022-09-27 01:16:57 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,9 +5,12 @@
  * @b: place holder
  * @n: number of bytes
  * @s: file tobe replace
+ * Return: s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i;
+
 	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b;
