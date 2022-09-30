ac4d47e6912d20784479517678046c8069e8ca5f
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2022-09-30 02:33:17 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * @argc: argument count
+ * @argv: argument variable
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int c;
+
+	for (c = 0; c < argc; c++)
+	{
+		printf("%s\n", argv[c]);
+	}
+	return (0);
+}
