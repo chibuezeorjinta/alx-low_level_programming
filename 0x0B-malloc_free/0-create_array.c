0e0bcdbac9d1a857ea371073300181c62dd549af
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2022-10-03 02:46:48 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <mem.h>
+
+/**
+ * create_array - Entey
+ * @size: Required size of the array
+ * @c: First character of array
+ * Return: if size = 0b return NULL, else return array, else return NULL.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+
+	if (size == 0)
+		return (NULL);
+	else (size != 0)
+	{
+		array = malloc(size * sizeof(char));
+	}
+
+	return (array);
+	else
+		return (NULL);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Learning to dynamically allocate memory
