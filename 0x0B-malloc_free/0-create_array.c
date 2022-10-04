9e2ef22b2e62933993b5cabf31debace94ac16da
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2022-10-04 03:44:36 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,13 +15,11 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	else
-	{
-		array = malloc(size * sizeof(char));
 
-		if (array == NULL)
-			return (NULL);
-	}
+	array = malloc(size * sizeof(char));
+
+	if (array == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		array[i] = c;
