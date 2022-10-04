8a85b6df19f299952252cf1d602689cc568143e3
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2022-10-04 07:54:47 -0700

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - Entry
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int c, b, i, k, j;
+	char *a;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		i = i;
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		j = j;
+	}
+	c = i + j;
+	k = c;
+	b = 0;
+
+	a = malloc(c * sizeof(char));
+	if (a == NULL)
+		return (NULL);
+	for (c = 0; c < i; c++)
+		a[c] = s1[c];
+	for (c = i; c < k; c++)
+	{
+		a[c] = s2[b];
+		b++;
+	}
+	a[c] = '\0';
+	return (a);
+}
