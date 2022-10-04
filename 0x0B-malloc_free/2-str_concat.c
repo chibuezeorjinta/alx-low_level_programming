ca959805c426677c868bcb808f9f20ff76054783
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2022-10-04 08:16:32 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,49 +1,68 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * str_concat - Entry
- * @s1: first string
- * @s2: second string
- * Return: pointer
- */
+* _strlen - returns the lenght of a string
+*@s: poiter of character
+*Return: the length of a string
+*/
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
+
+/**
+* *str_concat - concatenates two strings
+*@s1: first string to concatenate
+*@s2: second string to concatenate
+*Return: the ponter to string concatenate or NULL
+*/
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int c, b, i, k, j;
-	char *a;
+	int l1, l2;
+	int i, j;
+	char *empty, *concat;
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		i = i;
-	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		j = j;
-	}
-	
+	/**lets make empty strings*/
+	empty = "";
+
+	/**comparing the enter strings*/
 	if (s1 == NULL)
-		c = j;
-	else if (s2 == NULL)
-		c = i;
-	else if (s1 == NULL && s2 == NULL)
-	{
-		a = NULL;
-	}
-	else
-		c = i + j;
-	k = c;
-	b = 0;
+		s1 = empty;
+	if (s2 == NULL)
+		s2 = empty;
 
-	a = malloc(c * sizeof(char));
-	if (a == NULL)
+	/**Calculating the length of the strings*/
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+
+	/**assign memory*/
+	/**increments l2 for the null character*/
+	l2++;
+	concat = malloc((l1 + l2) * sizeof(*concat));
+
+	/**verify if memory fails*/
+	if (concat == NULL)
 		return (NULL);
-	for (c = 0; c < i; c++)
-		a[c] = s1[c];
-	for (c = i; c < k; c++)
+
+	/**concatenate the strings*/
+	for (i = 0; i < l1; i++)
 	{
-		a[c] = s2[b];
-		b++;
+		concat[i] = s1[i];
 	}
-	a[c] = '\0';
-	return (a);
+
+	for (j = 0; j < l2; j++)
+	{
+		concat[i] = s2[j];
+		i++;
+	}
+	i++;
+	concat[i] = '\0';
+	return (concat);
 }
