67a13b818586dc221efc6e5ebc4d98ac48bf044f
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2022-10-10 05:37:04 -0700

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * init_dog - initialise struct
@@ -13,8 +14,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		;
 	else
 	{
-		d->name = *name;
+		d->name = name;
 		d->age = age;
-		d->owner = *owner;
+		d->owner = owner;
 	}
 }
