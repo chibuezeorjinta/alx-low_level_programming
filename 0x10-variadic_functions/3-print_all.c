fd6dfa63050a65f93742727738b6a7367ea4a37d
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2022-10-13 09:18:16 -0700

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,45 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - Entry
+ * @format
+ */
+void print_all(const char * const format, ...)
+{
+	int a;
+	va_list arg;
+	char *p;
+
+	va_start(arg, format);
+	a = 0;
+
+	while (format[a] != '\0')
+	{
+		switch (format[a])
+		{
+			case 'c':
+				printf("%c", (char)va_arg(arg, int));
+				break;
+			case 'i':
+				printf("%i", va_arg(arg, int));
+				break;
+			case 'f':
+				printf("%f", (float)va_arg(arg, double));
+				break;
+			case 's':
+				p = va_arg(arg,char *);
+				if (p != NULL)
+				{
+					printf("%s", p);
+					break;
+				}
+				printf("(nil)");
+				break;
+		}
+		if ((format[a] == 'c' || format[a] == 'i' || format[a] == 'f' || format[a] == 's') && format[a + 1] != '\0')
+			printf(", ");
+		a++;
+	}
+	printf("\n");
+}
