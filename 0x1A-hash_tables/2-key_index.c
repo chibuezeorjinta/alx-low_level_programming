a7158da41570c55f624e9567ca2dd94664a9f219
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-01-06 13:31:52 +0100

    betty again

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,13 +7,13 @@
  * @key: the key string to be used
  * @size: size of the hash table
  * Return: index on the hash table
-*/
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int h;
+	unsigned long int h;
 
-    h = hash_djb2(key);
-    if (size == 0)
-        return (EXIT_FAILURE);
-    return (h % size);
+	h = hash_djb2(key);
+	if (size == 0)
+		return (EXIT_FAILURE);
+	return (h % size);
 }
