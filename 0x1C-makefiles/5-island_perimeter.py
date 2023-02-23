28182ac0332f591b54f8c94131f6518a039b4736
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-02-23 04:22:59 -0800

    indent

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,19 +7,19 @@ def island_perimeter(grid):
     """
     finds the perimeter of a grid of 1's and 0's
     Args:
-        grid = Given grid
+	grid = Given grid
     """
-    width = len(grid[0])
-    height = len(grid)
-    edges = 0
-    size = 0
+	width = len(grid[0])
+	height = len(grid)
+	edges = 0
+	size = 0
 
-    for i in range(height):
-        for j in range(width):
-            if grid[i][j] == 1:
-                size += 1
-                if (j > 0 and grid[i][j - 1] == 1):
-                    edges += 1
-                if (i > 0 and grid[i - 1][j] == 1):
-                    edges += 1
-    return size * 4 - edges * 2
+	for i in range(height):
+		for j in range(width):
+			if grid[i][j] == 1:
+				size += 1
+				if (j > 0 and grid[i][j - 1] == 1):
+		    			edges += 1
+				if (i > 0 and grid[i - 1][j] == 1):
+					edges += 1
+	return size * 4 - edges * 2
